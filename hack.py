95de9e721476dc009a102be7e467117c039e51b8
Nikolay Chirgin <nickchirgin@gmail.com>
2021-12-19 16:08:26 +0300

    Second Commit

--- hack.py
+++ hack.py
@@ -1,15 +1,30 @@
 import socket
+import itertools
 import sys
-
+import string
 
 HOST = sys.argv[1]
 PORT = int(sys.argv[2])
 ADRESS = (HOST, PORT)
-password = sys.argv[3]
-password = password.encode()
+letter = list(string.ascii_lowercase)
+numbers = string.digits
+whole_list = list(itertools.chain(letter, numbers))
+flag = False
+password = ""
+n = 1
+
 with socket.socket() as s:
     s.connect(ADRESS)
-    s.send(password)
-    result = s.recv(1024)
-    result = result.decode()
-    print(result)
+    while not flag:
+        for item in itertools.product(whole_list, repeat=n):
+            password = "".join(item)
+            password = password.encode()
+            s.send(password)
+            result = s.recv(1024)
+            result = result.decode()
+            if result == "Connection success!":
+                password = password.decode()
+                print(password)
+                flag = True
+                break
+        n += 1
