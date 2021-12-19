7bb8f26230cc9af397bdfa0f693ad7e07865a590
Nikolay Chirgin <nickchirgin@gmail.com>
2021-12-19 16:57:34 +0300

    Third Commit

--- hack.py
+++ hack.py
@@ -2,29 +2,46 @@ import socket
 import itertools
 import sys
 import string
+import os
+
 
 HOST = sys.argv[1]
 PORT = int(sys.argv[2])
 ADRESS = (HOST, PORT)
-letter = list(string.ascii_lowercase)
 numbers = string.digits
-whole_list = list(itertools.chain(letter, numbers))
+whole_list = []
 flag = False
 password = ""
 n = 1
 
+
+def variant(file):
+    with open(file, "r") as f:
+        for pw in f.readlines():
+            pw = pw.strip("\n")
+            for variation in map(lambda x: ''.join(x), itertools.product(*([letter.lower(), letter.upper()] for letter in pw))):
+                yield variation
+
+
+var = variant("passwords.txt")
+
+
 with socket.socket() as s:
     s.connect(ADRESS)
     while not flag:
-        for item in itertools.product(whole_list, repeat=n):
-            password = "".join(item)
+        try:
+            password = next(var)
             password = password.encode()
             s.send(password)
-            result = s.recv(1024)
+            result = s.recv(10240)
             result = result.decode()
             if result == "Connection success!":
                 password = password.decode()
                 print(password)
                 flag = True
                 break
-        n += 1
+        except StopIteration:
+            print('All passwords have been attempted!')
+            break
+
+
